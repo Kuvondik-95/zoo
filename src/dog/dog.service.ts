cb57e98f457d4ff4cbb88693233dc48f3d65d8a6
Kuvondik-95 <kuvondik9595@gmail.com>
2025-06-04 13:59:02 +0900

    BRR: Initial setup

---
+++ src/dog/dog.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DogService {
+
+  public getHello(): string{
+    return "Woow Woow";
+  }
+  
+  public introduce(): string{
+    return "My name is REX!";
+  }
+
+}
